bac5532753cc42af4fb1466e8e0b9e8601c2577c
tranmanh2004 <manhsile@gmail.com>
2025-12-29 23:59:59 +0700

    update

---
+++ Assets/Scripts/UI/tutor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutor : MonoBehaviour
+{
+    public TextMeshProUGUI tutorText;
+    public List<String> tutorMessages;
+    public int currentMessageIndex = 0;
+    void Start()
+    {
+
+    }
+}
